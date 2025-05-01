453af53740eb697d697b3f279ae43d2d15ac7265
jd <jean.daniel.michaud@gmail.com>
2025-05-01 11:05:57 +0200

    misc: pass a .PX parameter to writePgm (flashback)

--- misc.zig
+++ misc.zig
@@ -19,7 +19,56 @@ pub fn load(pathname: []const u8) ![]align(4096) const u8 {
   return buffer;
 }
 
-pub fn writePgm(width: usize, height: usize, pixels: []const u8, filepath: []const u8) !void {
+
+const Ptype = enum {
+  P1, P2, P3, P4, P5, P6,
+};
+
+pub fn writePgm(comptime P: Ptype, width: usize, height: usize, pixels: []const u8, filepath: []const u8) !void {
+  if (width * height != pixels.len) {
+    return error.IncorrectSize;
+  }
+
+  // Create file
+  const file = try std.fs.cwd().createFile(
+    filepath,
+    .{ .read = true },
+  );
+  defer file.close();
+
+  switch (P) {
+    .P1 => {
+      // Prepare PGM header (https://en.wikipedia.org/wiki/Netpbm)
+      var buffer: [255]u8 = [_]u8{ 0 } ** 255;
+      const pgmHeader = try std.fmt.bufPrint(&buffer, "P1\n{} {}\n", .{ width, height });
+      // Write to file
+      try file.writeAll(pgmHeader);
+      for (0..height) |j| {
+        for (0..width) |i| {
+          if (pixels[j * width + i] != 0) {
+            try file.writeAll("1 ");
+          } else {
+            try file.writeAll("0 ");
+          }
+        }
+        try file.writeAll("\n");
+      }
+    },
+    .P6 => {
+      // Prepare PGM header (https://en.wikipedia.org/wiki/Netpbm)
+      var buffer: [255]u8 = [_]u8{ 0 } ** 255;
+      const pgmHeader = try std.fmt.bufPrint(&buffer, "P6\n{} {}\n255\n", .{ width, height });
+      // Write to file
+      try file.writeAll(pgmHeader);
+      const stdout = std.io.getStdOut().writer();
+      for (pixels) |p| { try stdout.print("{} ", .{ p }); }
+      try file.writeAll(pixels);
+    },
+    else => return error.NotImplemented,
+  }
+}
+
+pub fn writePam32(width: usize, height: usize, pixels: []const u8, filepath: []const u8) !void {
   // Create file
   const file = try std.fs.cwd().createFile(
     filepath,
@@ -28,7 +77,7 @@ pub fn writePgm(width: usize, height: usize, pixels: []const u8, filepath: []con
   defer file.close();
   // Prepare PGM header (https://en.wikipedia.org/wiki/Netpbm)
   var buffer: [255]u8 = [_]u8{ 0 } ** 255;
-  const pgmHeader = try std.fmt.bufPrint(&buffer, "P6\n{} {}\n255\n", .{ width, height });
+  const pgmHeader = try std.fmt.bufPrint(&buffer, "P7\nWIDTH {}\nHEIGHT {}\nDEPTH 4\nMAXVAL 255\nTUPLTYPE RGB_ALPHA\nENDHDR\n", .{ width, height });
   // Write to file
   try file.writeAll(pgmHeader);
   try file.writeAll(pixels);
